236c1ea50bccf549320664fb98ca7630a11b7d31
Casiel Didriksson <casield@gmail.com>
2019-10-02 01:16:30 -0500

    updated mongodb

--- src/passport/conexionDB.js
+++ src/passport/conexionDB.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/FoodAdventures")
+mongoose.connect("mongodb://casiel:Drokirifa123@localhost:27017/FoodAdventures")
+//mongoose.connect("mongodb://casiel:Drokirifa123@localhost:27017/FoodAdventures")
 .then(db => console.log("Base de datos conectada"))
 .catch(err => console.log(err));
 
